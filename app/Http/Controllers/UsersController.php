4d4e58f4403fc912424d5aba57576a16a0cb4470
xgjin <2210455042@qq.com>
2019-08-29 09:37:50 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -112,13 +112,18 @@ class UsersController extends Controller
     {
         $view    = 'emails.confirm';
         $data    = compact('user');
-        $from    = '2210455042@qq.com';
-        $name    = 'Kim';
+        // $from    = '2210455042@qq.com';
+        // $name    = 'Kim';
         $to      = $user->email;
         $subject = '感谢注册 Weibo 应用！请确认你的邮箱。';
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        // Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+        //     $message->from($from, $name)->to($to)->subject($subject);
+        // });
+
+        //$form,$name 从.env配置里读取
+         Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 }
